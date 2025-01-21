b9aea1dd746ede9e0fedabaecee68431562bb4ae
xtkcx <168980047+xtkcx@users.noreply.github.com>
2025-01-22 00:21:09 +0900

    アカウント登録完了画面の作成

---
+++ regist_complete.php
@@ -0,0 +1,20 @@
+<!doctype HTML>
+<html lang="ja">
+<head>
+<meta charset="utf-8">
+<title>アカウント登録完了画面
+<link rel="stylesheet" type="text/css" href="regist_complete.css">
+</head>
+<body>
+<h1>アカウント登録完了画面</h1>
+
+<div class="confirm">
+<h2>登録完了しました</h2>
+</div>
+
+<form action="D.I.blog.php">
+<input type="submit" class="button1" value="TOPページへ戻る">		
+</form>
+
+</body>
+</html>
