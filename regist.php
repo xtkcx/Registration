6717a0b6e50298fe52b8c8199061e3e8892944e3
xtkcx <168980047+xtkcx@users.noreply.github.com>
2025-08-28 22:51:50 +0900

    バリデーション(赤字)の追加

--- regist.php
+++ regist.php
@@ -1,73 +1,94 @@
 <?php
 session_start();
-$form = $_SESSION['form'] ?? [];
+$errors = $_SESSION['errors'] ?? [];
+$form   = $_SESSION['form']   ?? [];
 ?>
-
 <!doctype HTML>
 <html lang="ja">
-
 <head>
     <meta charset="utf-8">
     <title>アカウント登録</title>
-    <link rel="stylesheet" type="text/css" href="regist.css">
+    <style>
+        .error { color: red; font-size: 0.9em; }
+    </style>
 </head>
 
 <body>
-    <header>
-        <div class="navi">
-            ナビゲーションバー
-        </div>
-    </header>
     <h1>アカウント登録画面</h1>
 
     <form method="post" action="regist_confirm.php">
-
         <div>
             <label>名前（姓）</label>
-            <input type="text" class="text" size="10" name="family_name" maxlength="10"
+            <input type="text" name="family_name" maxlength="10"
                    value="<?= $form['family_name'] ?? '' ?>">
+            <?php if(isset($errors['family_name'])): ?>
+                <p class="error"><?= $errors['family_name'] ?></p>
+            <?php endif; ?>
         </div>
 
         <div>
             <label>名前（名）</label>
-            <input type="text" class="text" size="10" name="last_name" maxlength="10"
+            <input type="text" name="last_name" maxlength="10"
                    value="<?= $form['last_name'] ?? '' ?>">
+            <?php if(isset($errors['last_name'])): ?>
+                <p class="error"><?= $errors['last_name'] ?></p>
+            <?php endif; ?>
         </div>
 
         <div>
             <label>カナ（姓）</label>
-            <input type="text" class="text" size="10" name="family_name_kana" maxlength="10"
+            <input type="text" name="family_name_kana" maxlength="10"
                    value="<?= $form['family_name_kana'] ?? '' ?>">
+            <?php if(isset($errors['family_name_kana'])): ?>
+                <p class="error"><?= $errors['family_name_kana'] ?></p>
+            <?php endif; ?>
         </div>
 
         <div>
             <label>カナ（名）</label>
-            <input type="text" class="text" size="10" name="last_name_kana" maxlength="10"
+            <input type="text" name="last_name_kana" maxlength="10"
                    value="<?= $form['last_name_kana'] ?? '' ?>">
+            <?php if(isset($errors['last_name_kana'])): ?>
+                <p class="error"><?= $errors['last_name_kana'] ?></p>
+            <?php endif; ?>
         </div>
 
         <div>
             <label>メールアドレス</label>
-            <input type="text" class="text" size="10" name="mail" maxlength="100"
+            <input type="text" name="mail" maxlength="100"
                    value="<?= $form['mail'] ?? '' ?>">
+            <?php if(isset($errors['mail'])): ?>
+                <p class="error"><?= $errors['mail'] ?></p>
+            <?php endif; ?>
         </div>
 
         <div>
             <label>パスワード</label>
-            <input type="password" class="text" size="10" name="password" maxlength="10"
+            <input type="password" name="password" maxlength="10"
                    value="<?= $form['password'] ?? '' ?>">
+            <?php if(isset($errors['password'])): ?>
+                <p class="error"><?= $errors['password'] ?></p>
+            <?php endif; ?>
         </div>
 
         <div>
             <label>性別</label>
-            <label><input type="radio" name="gender" value="0" <?= (isset($form['gender']) && $form['gender'] === "0") ? "checked" : "" ?>>男</label>
-            <label><input type="radio" name="gender" value="1" <?= (isset($form['gender']) && $form['gender'] === "1") ? "checked" : "" ?>>女</label>
+            <label><input type="radio" name="gender" value="0"
+                <?= ($form['gender'] ?? '0') === '0' ? 'checked' : '' ?>>男</label>
+            <label><input type="radio" name="gender" value="1"
+                <?= ($form['gender'] ?? '') === '1' ? 'checked' : '' ?>>女</label>
+            <?php if(isset($errors['gender'])): ?>
+                <p class="error"><?= $errors['gender'] ?></p>
+            <?php endif; ?>
         </div>
 
         <div>
             <label>郵便番号</label>
-            <input type="text" class="text" size="10" name="postal_code" maxlength="7"
+            <input type="text" name="postal_code" maxlength="7"
                    value="<?= $form['postal_code'] ?? '' ?>">
+            <?php if(isset($errors['postal_code'])): ?>
+                <p class="error"><?= $errors['postal_code'] ?></p>
+            <?php endif; ?>
         </div>
 
         <div>
@@ -83,43 +104,48 @@ $form = $_SESSION['form'] ?? [];
                           "徳島県","香川県","愛媛県","高知県","福岡県","佐賀県","長崎県",
                           "熊本県","大分県","宮崎県","鹿児島県","沖縄県"];
                 foreach ($prefs as $pref) {
-                    $selected = ($form['prefecture'] ?? '') === $pref ? "selected" : "";
+                    $selected = ($form['prefecture'] ?? '') === $pref ? 'selected' : '';
                     echo "<option value='{$pref}' {$selected}>{$pref}</option>";
                 }
                 ?>
             </select>
+            <?php if(isset($errors['prefecture'])): ?>
+                <p class="error"><?= $errors['prefecture'] ?></p>
+            <?php endif; ?>
         </div>
 
         <div>
             <label>住所（市区町村）</label>
-            <input type="text" class="text" size="10" name="address_1" maxlength="10"
+            <input type="text" name="address_1" maxlength="10"
                    value="<?= $form['address_1'] ?? '' ?>">
+            <?php if(isset($errors['address_1'])): ?>
+                <p class="error"><?= $errors['address_1'] ?></p>
+            <?php endif; ?>
         </div>
 
         <div>
             <label>住所（番地）</label>
-            <input type="text" class="text" size="10" name="address_2" maxlength="100"
+            <input type="text" name="address_2" maxlength="100"
                    value="<?= $form['address_2'] ?? '' ?>">
+            <?php if(isset($errors['address_2'])): ?>
+                <p class="error"><?= $errors['address_2'] ?></p>
+            <?php endif; ?>
         </div>
 
         <div>
             <label>アカウント権限</label>
             <select name="authority">
-                <option value="0" <?= ($form['authority'] ?? '') === "0" ? "selected" : "" ?>>一般</option>
-                <option value="1" <?= ($form['authority'] ?? '') === "1" ? "selected" : "" ?>>管理者</option>
+                <option value="0" <?= ($form['authority'] ?? '0') === '0' ? 'selected' : '' ?>>一般</option>
+                <option value="1" <?= ($form['authority'] ?? '') === '1' ? 'selected' : '' ?>>管理者</option>
             </select>
+            <?php if(isset($errors['authority'])): ?>
+                <p class="error"><?= $errors['authority'] ?></p>
+            <?php endif; ?>
         </div>
 
         <div>
             <button type="submit">確認する</button>
         </div>
-
     </form>
-
-    <footer>
-        <div class="footer">
-            フッター
-        </div>
-    </footer>
 </body>
 </html>
--- regist_confirm.php
+++ regist_confirm.php
@@ -1,10 +1,105 @@
-<!doctype HTML>
+<?php
+session_start();
+
+$errors = [];
+
+if (empty($_POST['family_name'])) {
+    $errors['family_name'] = '名前（姓）が未入力です。';
+} elseif (!preg_match('/^[\p{Hiragana}\p{Han}]+$/u', $_POST['family_name'])) {
+    $errors['family_name'] = '名前（姓）はひらがな・漢字のみ入力できます。';
+} elseif (mb_strlen($_POST['family_name']) > 10) {
+    $errors['family_name'] = '名前（姓）は10文字以内で入力してください。';
+}
+
+if (empty($_POST['last_name'])) {
+    $errors['last_name'] = '名前（名）が未入力です。';
+} elseif (!preg_match('/^[\p{Hiragana}\p{Han}]+$/u', $_POST['last_name'])) {
+    $errors['last_name'] = '名前（名）はひらがな・漢字のみ入力できます。';
+} elseif (mb_strlen($_POST['last_name']) > 10) {
+    $errors['last_name'] = '名前（名）は10文字以内で入力してください。';
+}
+
+if (empty($_POST['family_name_kana'])) {
+    $errors['family_name_kana'] = 'カナ（姓）が未入力です。';
+} elseif (!preg_match('/^[\p{Katakana}]+$/u', $_POST['family_name_kana'])) {
+    $errors['family_name_kana'] = 'カナ（姓）はカタカナのみ入力できます。';
+} elseif (mb_strlen($_POST['family_name_kana']) > 10) {
+    $errors['family_name_kana'] = 'カナ（姓）は10文字以内で入力してください。';
+}
+
+if (empty($_POST['last_name_kana'])) {
+    $errors['last_name_kana'] = 'カナ（名）が未入力です。';
+} elseif (!preg_match('/^[\p{Katakana}]+$/u', $_POST['last_name_kana'])) {
+    $errors['last_name_kana'] = 'カナ（名）はカタカナのみ入力できます。';
+} elseif (mb_strlen($_POST['last_name_kana']) > 10) {
+    $errors['last_name_kana'] = 'カナ（名）は10文字以内で入力してください。';
+}
+
+if (empty($_POST['mail'])) {
+    $errors['mail'] = 'メールアドレスが未入力です。';
+} elseif (!preg_match('/^[A-Za-z0-9._%+\-]+@[A-Za-z0-9.\-]+\.[A-Za-z]{2,}$/', $_POST['mail'])) {
+    $errors['mail'] = 'メールアドレスの形式が正しくありません。';
+} elseif (mb_strlen($_POST['mail']) > 100) {
+    $errors['mail'] = 'メールアドレスは100文字以内で入力してください。';
+}
+
+if (empty($_POST['password'])) {
+    $errors['password'] = 'パスワードが未入力です。';
+} elseif (!preg_match('/^[0-9A-Za-z]+$/', $_POST['password'])) {
+    $errors['password'] = 'パスワードは半角英数字のみ入力できます。';
+} elseif (mb_strlen($_POST['password']) > 10) {
+    $errors['password'] = 'パスワードは10文字以内で入力してください。';
+}
+
+if (!isset($_POST['gender'])) {
+    $errors['gender'] = '性別が未入力です。';
+}
+
+if (empty($_POST['postal_code'])) {
+    $errors['postal_code'] = '郵便番号が未入力です。';
+} elseif (!preg_match('/^[0-9]{7}$/', $_POST['postal_code'])) {
+    $errors['postal_code'] = '郵便番号は半角数字7桁で入力してください。';
+}
+
+if (empty($_POST['prefecture'])) {
+    $errors['prefecture'] = '住所（都道府県）が未入力です。';
+}
+
+if (empty($_POST['address_1'])) {
+    $errors['address_1'] = '住所（市区町村）が未入力です。';
+} elseif (!preg_match('/^[\p{Hiragana}\p{Katakana}\p{Han}0-9 \-]+$/u', $_POST['address_1'])) {
+    $errors['address_1'] = '住所（市区町村）はひらがな・カタカナ・漢字・数字・スペース・ハイフンのみ入力できます。';
+} elseif (mb_strlen($_POST['address_1']) > 10) {
+    $errors['address_1'] = '住所（市区町村）は10文字以内で入力してください。';
+}
+
+if (empty($_POST['address_2'])) {
+    $errors['address_2'] = '住所（番地）が未入力です。';
+} elseif (!preg_match('/^[\p{Hiragana}\p{Katakana}\p{Han}0-9 \-]+$/u', $_POST['address_2'])) {
+    $errors['address_2'] = '住所（番地）はひらがな・カタカナ・漢字・数字・スペース・ハイフンのみ入力できます。';
+} elseif (mb_strlen($_POST['address_2']) > 100) {
+    $errors['address_2'] = '住所（番地）は100文字以内で入力してください。';
+}
+
+if (!isset($_POST['authority'])) {
+    $errors['authority'] = 'アカウント権限が未入力です。';
+}
+
+if (!empty($errors)) {
+    $_SESSION['errors'] = $errors;
+    $_SESSION['form']   = $_POST;
+    header('Location: regist.php');
+    exit;
+}
+
+$_SESSION['form'] = $_POST;
+?>
+<!doctype html>
 <html lang="ja">
 
 <head>
     <meta charset="utf-8">
     <title>アカウント登録確認画面</title>
-    <link rel="stylesheet" type="text/css" href="regist_confirm.css">
 </head>
 
 <body>
@@ -23,40 +118,8 @@
         <p>住所（番地）<?php echo $_POST['address_2']; ?></p>
         <p>アカウント権限<?php echo $_POST['authority'] === "0" ? "一般" : "管理者"; ?></p>
 
-        <form action="regist.php">
-            <form action="regist.php" method="post">
-                <input type="hidden" name="family_name" value="<?php echo $_POST['family_name']; ?>">
-                <input type="hidden" name="last_name" value="<?php echo $_POST['last_name']; ?>">
-                <input type="hidden" name="family_name_kana" value="<?php echo $_POST['family_name_kana']; ?>">
-                <input type="hidden" name="last_name_kana" value="<?php echo $_POST['last_name_kana']; ?>">
-                <input type="hidden" name="mail" value="<?php echo $_POST['mail']; ?>">
-                <input type="hidden" name="password" value="<?php echo $_POST['password']; ?>">
-                <input type="hidden" name="gender" value="<?php echo $_POST['gender']; ?>">
-                <input type="hidden" name="postal_code" value="<?php echo $_POST['postal_code']; ?>">
-                <input type="hidden" name="prefecture" value="<?php echo $_POST['prefecture']; ?>">
-                <input type="hidden" name="address_1" value="<?php echo $_POST['address_1']; ?>">
-                <input type="hidden" name="address_2" value="<?php echo $_POST['address_2']; ?>">
-                <input type="hidden" name="authority" value="<?php echo $_POST['authority']; ?>">
-
-                <input type="submit" class="button1" value="前に戻る">
-            </form>
-        </form>
-        <form action="regist_complete.php" method="post">
-            <input type="hidden" value="<?php echo $_POST['family_name']; ?>" name="family_name">
-            <input type="hidden" value="<?php echo $_POST['last_name']; ?>" name="last_name">
-            <input type="hidden" value="<?php echo $_POST['family_name_kana']; ?>" name="family_name_kana">
-            <input type="hidden" value="<?php echo $_POST['last_name_kana']; ?>" name="last_name_kana">
-            <input type="hidden" value="<?php echo $_POST['mail']; ?>" name="mail">
-            <input type="hidden" value="<?php echo $_POST['password']; ?>" name="password">
-            <input type="hidden" value="<?php echo $_POST['gender']; ?>" name="gender">
-            <input type="hidden" value="<?php echo $_POST['postal_code']; ?>" name="postal_code">
-            <input type="hidden" value="<?php echo $_POST['prefecture']; ?>" name="prefecture">
-            <input type="hidden" value="<?php echo $_POST['address_1']; ?>" name="address_1">
-            <input type="hidden" value="<?php echo $_POST['address_2']; ?>" name="address_2">
-            <input type="hidden" value="<?php echo $_POST['authority']; ?>" name="authority">
-
-            <input type="submit" class="button2" value="登録する">
-        </form>
+        <form action="regist.php" method="post"><button>前に戻る</button></form>
+        <form action="regist_complete.php" method="post"><button>登録する</button></form>
     </div>
 </body>
 
